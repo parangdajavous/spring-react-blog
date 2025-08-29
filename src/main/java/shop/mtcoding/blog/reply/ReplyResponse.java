26370f15311cee3cdfcb1677b7e4341eef1f7149
parangdajavous <nuit0535@gmail.com>
2025-08-29 10:48:18 +0900

    서버 수정

--- src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyResponse.java
@@ -6,13 +6,35 @@ import lombok.Data;
 public class ReplyResponse {
 
     @Data
-    public static class SaveDTO {
+    public static class DTO {
+        private Integer replyId;
         private Integer boardId;
+        private Integer userId;
         private String comment;
 
-        public SaveDTO(Integer boardId, Reply reply) {
-            this.boardId = boardId;
+
+        public DTO(Reply reply) {
+            this.replyId = reply.getId();
+            this.boardId = reply.getBoard().getId();
+            this.userId = reply.getUser().getId();
             this.comment = reply.getComment();
+        }
+    }
+
+    @Data
+    public static class DetailDTO {
+        private Integer id;
+        private String comment;
+        private Integer userId;
+        private String username;
+        private Boolean owner;
+
+        public DetailDTO(Reply reply, Integer sessionUserId) {
+            this.id = reply.getId();
+            this.comment = reply.getComment();
+            this.userId = reply.getUser().getId(); // lazy loading 안 됨
+            this.username = reply.getUser().getUsername(); // lazy loading 안 됨 (user 영속화 되어있음)
+            this.owner = reply.getUser().getId() == sessionUserId;
         }
     }
 }
